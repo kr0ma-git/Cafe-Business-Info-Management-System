f024532b2d1a12109715a32a4979a8a499ea5e4d
kr0ma-git <w4ynekousuke@gmail.com>
2025-04-27 23:12:36 +0800

    Updated SQL Database 27/04/2025

---
+++ SQL backup/27,04,2025/thebeanstalkdb.sql
@@ -0,0 +1,275 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: Apr 27, 2025 at 05:11 PM
+-- Server version: 10.4.32-MariaDB
+-- PHP Version: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `thebeanstalkdb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cart`
+--
+
+CREATE TABLE `cart` (
+  `cartID` int(11) NOT NULL,
+  `customerID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `quantity` int(11) DEFAULT 1,
+  `added_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `company`
+--
+
+CREATE TABLE `company` (
+  `ID` int(11) NOT NULL,
+  `Role` enum('admin','employee') NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `ContactNumber` varchar(20) NOT NULL,
+  `Salary` decimal(10,2) NOT NULL,
+  `Department` varchar(50) DEFAULT NULL,
+  `Status` enum('active','disabled') NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `company`
+--
+
+INSERT INTO `company` (`ID`, `Role`, `FirstName`, `LastName`, `Email`, `Password`, `ContactNumber`, `Salary`, `Department`, `Status`) VALUES
+(1, 'admin', 'testAdmin', 'testAdmin', 'testAdmin@gmail.com', '$2a$12$XCYKnws1Jz/CKRs7844GjeV1zucMYE9ZmzL0kw7wu5gWimuZhK3vG', '1234567890', 50000.00, 'IT', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customers`
+--
+
+CREATE TABLE `customers` (
+  `ID` int(11) NOT NULL,
+  `FirstName` varchar(50) NOT NULL,
+  `LastName` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `ContactNumber` varchar(20) NOT NULL,
+  `Status` enum('active','disabled') NOT NULL DEFAULT 'active'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customers`
+--
+
+INSERT INTO `customers` (`ID`, `FirstName`, `LastName`, `Email`, `Password`, `ContactNumber`, `Status`) VALUES
+(1, 'test', 'test', 'test@gmail.com', '$2y$10$pZ.5xjMKjy/.ETB5mtJnSuxhFXXWSlmPj52Ko5AmVX3WMTVEMr3r2', '123', 'active');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `customer_orders`
+--
+
+CREATE TABLE `customer_orders` (
+  `orderID` int(11) NOT NULL,
+  `customerID` int(11) NOT NULL,
+  `orderDatetime` timestamp NOT NULL DEFAULT current_timestamp(),
+  `totalAmount` decimal(10,2) NOT NULL,
+  `paymentMethod` varchar(50) DEFAULT NULL,
+  `contactNumber` varchar(20) DEFAULT NULL,
+  `status` varchar(20) DEFAULT 'Pending'
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `customer_orders`
+--
+
+INSERT INTO `customer_orders` (`orderID`, `customerID`, `orderDatetime`, `totalAmount`, `paymentMethod`, `contactNumber`, `status`) VALUES
+(1, 1, '2025-04-27 14:10:41', 240.00, NULL, NULL, 'Pending'),
+(2, 1, '2025-04-27 14:42:18', 240.00, 'Cash on Deliver', '123', 'Pending'),
+(3, 1, '2025-04-27 14:43:16', 240.00, 'Cash on Deliver', '123', 'Pending'),
+(4, 1, '2025-04-27 15:05:34', 240.00, 'Cash on Deliver', '123', 'Pending'),
+(5, 1, '2025-04-27 15:08:41', 120.00, 'Cash on Deliver', '123', 'Pending');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `menu_items`
+--
+
+CREATE TABLE `menu_items` (
+  `itemID` int(11) NOT NULL,
+  `name` varchar(100) NOT NULL,
+  `description` text DEFAULT NULL,
+  `price` decimal(10,2) NOT NULL,
+  `stock` int(11) DEFAULT 0,
+  `image` varchar(255) DEFAULT 'default.jpg',
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `menu_items`
+--
+
+INSERT INTO `menu_items` (`itemID`, `name`, `description`, `price`, `stock`, `image`, `created_at`) VALUES
+(1, 'Café Latte', 'Rich espresso with steamed milk.', 120.00, 5, '../images/pexels-juanpphotoandvideo-894696 (1).jpg', '2025-04-06 06:29:30'),
+(2, 'Mocha', 'Chocolate-flavored espresso milk.', 135.00, 0, '../images/pexels-tyler-nix-1259808-2396220.jpg', '2025-04-06 06:29:30');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `order_items`
+--
+
+CREATE TABLE `order_items` (
+  `orderItemID` int(11) NOT NULL,
+  `orderID` int(11) NOT NULL,
+  `itemID` int(11) NOT NULL,
+  `quantity` int(11) NOT NULL,
+  `priceAtPurchase` decimal(10,2) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `order_items`
+--
+
+INSERT INTO `order_items` (`orderItemID`, `orderID`, `itemID`, `quantity`, `priceAtPurchase`) VALUES
+(1, 3, 1, 2, 120.00),
+(2, 4, 1, 2, 120.00),
+(3, 5, 1, 1, 120.00);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cart`
+--
+ALTER TABLE `cart`
+  ADD PRIMARY KEY (`cartID`),
+  ADD KEY `customerID` (`customerID`),
+  ADD KEY `itemID` (`itemID`);
+
+--
+-- Indexes for table `company`
+--
+ALTER TABLE `company`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `customers`
+--
+ALTER TABLE `customers`
+  ADD PRIMARY KEY (`ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- Indexes for table `customer_orders`
+--
+ALTER TABLE `customer_orders`
+  ADD PRIMARY KEY (`orderID`),
+  ADD KEY `customerID` (`customerID`);
+
+--
+-- Indexes for table `menu_items`
+--
+ALTER TABLE `menu_items`
+  ADD PRIMARY KEY (`itemID`);
+
+--
+-- Indexes for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD PRIMARY KEY (`orderItemID`),
+  ADD KEY `orderID` (`orderID`),
+  ADD KEY `itemID` (`itemID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cart`
+--
+ALTER TABLE `cart`
+  MODIFY `cartID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=10;
+
+--
+-- AUTO_INCREMENT for table `company`
+--
+ALTER TABLE `company`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customers`
+--
+ALTER TABLE `customers`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- AUTO_INCREMENT for table `customer_orders`
+--
+ALTER TABLE `customer_orders`
+  MODIFY `orderID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=6;
+
+--
+-- AUTO_INCREMENT for table `menu_items`
+--
+ALTER TABLE `menu_items`
+  MODIFY `itemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `order_items`
+--
+ALTER TABLE `order_items`
+  MODIFY `orderItemID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `cart`
+--
+ALTER TABLE `cart`
+  ADD CONSTRAINT `cart_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customers` (`ID`),
+  ADD CONSTRAINT `cart_ibfk_2` FOREIGN KEY (`itemID`) REFERENCES `menu_items` (`itemID`);
+
+--
+-- Constraints for table `customer_orders`
+--
+ALTER TABLE `customer_orders`
+  ADD CONSTRAINT `customer_orders_ibfk_1` FOREIGN KEY (`customerID`) REFERENCES `customers` (`ID`);
+
+--
+-- Constraints for table `order_items`
+--
+ALTER TABLE `order_items`
+  ADD CONSTRAINT `order_items_ibfk_1` FOREIGN KEY (`orderID`) REFERENCES `customer_orders` (`orderID`),
+  ADD CONSTRAINT `order_items_ibfk_2` FOREIGN KEY (`itemID`) REFERENCES `menu_items` (`itemID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
